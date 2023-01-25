0e7360cb71ce4e45929823545fb8245bd1260ba6
Gudmon <gudmonmarcellwork@gmail.com>
2023-01-25 10:59:21 +0100

    Add: tailwind. config, google-fonts, components

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <p>Future Portfolio website</p>
+      <p>Test</p>
     </div>
   )
 }
