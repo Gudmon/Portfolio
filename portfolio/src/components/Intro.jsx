b437e8b630338bf0b62ea2718acfef78ad18d265
Gudmon <gudmonmarcellwork@gmail.com>
2023-01-26 15:53:49 +0100

    Edit: Intro message

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <h1 className="text-4xl md:text-7xl text-rose-600 dark:text-white mb-1 md:mb-3 font-bold">! This page is under construction !</h1>
         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Marcell Gudmon</h1>
-        <p className="text-base md:text-xl mb-3 font-medium">Junior Developer</p>
+        <p className="text-base md:text-xl mb-4 font-medium">Junior Developer</p>
         <p className="text-sm max-w-xl mb-6 font-bold">
         I consider myself as a highly
         motivated and commited
