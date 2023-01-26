b93a414d4da2ecf1b0d4420243cb9e1b81ff4baf
Gudmon <gudmonmarcellwork@gmail.com>
2023-01-26 21:15:32 +0100

    Edit: Download cv button style

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from "@material-tailwind/react";
 
 function Intro() {
    return (
@@ -11,8 +12,11 @@ function Intro() {
          I am enthusiastic about my work and always looking for new challenges to improve my craft.<br/>
          I hope you enjoy exploring my portfolio and feel free to reach out with any opportunities or questions.<br/>      
         </p>
-        <a href="/assets/CV_GM_2.pdf" download="CV_GM.pdf"> 
-         <button>CV</button>
+        <a href="/assets/CV_GM_2.pdf" download="CV_GM.pdf">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+               <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+               <span>Download CV</span>
+         </button> 
         </a>
       </div>
    )
