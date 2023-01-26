12587d98eaa09121fe0983bc7c839b53e357ce75
Gudmon <gudmonmarcellwork@gmail.com>
2023-01-26 18:18:26 +0100

    Add: Timeline items with formatting

--- portfolio/src/components/Intro.jsx
+++ portfolio/src/components/Intro.jsx
@@ -3,20 +3,13 @@ import React from 'react';
 function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Marcell Gudmon</h1>
-        <p className="text-base md:text-xl mb-4 font-medium">Junior Developer</p>
-        <p className="text-sm max-w-xl mb-6 font-bold">
-        I consider myself as a highly
-        motivated and commited
-        person with a strong passion for
-        coding and solution-oriented mindset.
-        This has led me to
-        develop my skills mainly in C#
-        and Javascript. I started
-        coding with Python first, besides
-        I am always happy and open to learn
-        new technologies and
-        techniques to improve and deepen my knowledge.        
+        <h1 className="text-4xl md:text-6xl mb-1 md:mb-3 font-bold">Marcell Gudmon</h1>
+        <h2 className="text-2xl md:text-3xl mb-3 my-3 font-medium">Junior Developer</h2>
+        <p className="text-xl max-w-2xl mb-5 my-5 font-bold">
+         Hello, my name is Marcell Gudmon and I am a junior full-stack developer with a passion for learning new technologies.<br/>
+         I have been honing my skills for over a year now.<br/>
+         I am enthusiastic about my work and always looking for new challenges to improve my craft.<br/>
+         I hope you enjoy exploring my portfolio and feel free to reach out with any opportunities or questions.<br/>      
         </p>
       </div>
    )
--- portfolio/src/data/timeline.js
+++ portfolio/src/data/timeline.js
@@ -1,8 +1,26 @@
 export default [
     {
-        year: "2022 - ",
-        title: "Full-stack Developer Student @ Codecool",
-        duration: "1 year",
-        details: "",
-    }
+        year: "Nov 2022 - ",
+        title: "ASP.NET Core @ Codecool",
+        duration: "4 months",
+        details: "Continued working with C# in the .NET framework. Things got even more interesting with MVC, Entity Framework, LINQ, Basic Authentication and Authorization, Docker and Microsoft Azure.",
+    },
+    {
+        year: "Aug 2022 - Nov 2022",
+        title: "OOP with C# @ Codecool",
+        duration: "4 months",
+        details: "This was a huge step from functional programming to object oriented programming. Learned new things, such as OOP principles, access modifiers, basic algorithms and much more.",
+    },
+    {
+        year: "May 2022 - Aug 2022",
+        title: "Web & SQL with Python Flask @ Codecool",
+        duration: "4 months",
+        details: "Started working on the web with Python Flask (Jinja2), PostgreSQL, JavaScript, HTML, CSS, Bootstrap. Learned how the basic HTTP requests, JavaScript DOM manipulation, Promises work.",
+    },
+    {
+        year: "Feb 2022 - May 2022",
+        title: "Programming Basics @ Codecool",
+        duration: "4 months",
+        details: "Wrote my first lines of code with Pyton. This is where I got familiar with the basic data types, Git and GitHub. We wrote single console applications, mainly games (Tic Tac Toe, Battleship, Roguelike Game). It was really fun!",
+    },
 ]
